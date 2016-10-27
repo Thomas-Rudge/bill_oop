13a8434876202b0cde05a6d76cab6bba3304e88b
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-10-27 15:24:30 +0100

    Updated functionality
    Most of it is now working. I still need to add support for discounts. Havent fully tested yet

--- bill.rb
+++ bill.rb
@@ -7,8 +7,8 @@ class EOS
   ##
   ## enforce_locales should be set to false if the money 
   ## gem complains that your locale is not valid
-  def initialize(ccy='GBP', enforce_locales=false, ref_start=0)
-    @ccy = ccy
+  def initialize(ccy='gbp', enforce_locales:false, ref_start:0)
+    @ccy = ccy.downcase! || ccy
     @ref = ref_start
     I18n.enforce_available_locales = enforce_locales
     @bill_list = {}
@@ -20,8 +20,8 @@ class EOS
     return Bill.new(self, @ref)
   end
   ## This creates a new item within the system, which will be available to bills
-  def new_item
-    return Item.new(self, name, price, discount, tax=0, tags=[], price_include_vat=true)
+  def new_item(name, price, discount:false, tax:0, tags:[], price_include_vat:true)
+    return Item.new(self, name, price, discount, tax, tags, price_include_vat)
   end
   ## Submits the bill to the systems bill list. Should be called from bill
   def submit(bill)
@@ -43,7 +43,7 @@ class EOS
   end
   
   private :submit
-  attr_reader :bill_list, :ref, :system_total
+  attr_reader :bill_list, :ref, :system_total, :ccy
 end
 
 class Bill
@@ -51,7 +51,8 @@ class Bill
   ## bill_ref is a system unique id
   def initialize(eos, bill_ref)
     @eos = eos
-    @subtotal = Money.new(0, @@ccy)
+    @subtotal = Money.new(0, eos.ccy)
+    @tax = Money.new(0, eos.ccy)
     @bill_ref = bill_ref
     # items format {name: [item, quantity]}
     @items = {}
@@ -59,11 +60,13 @@ class Bill
   end
   ## Adds an item to the bill.
   ## If already present increments quantity by 1
-  def add_item(item)
-    if @items.keys.include? item.name
-      @items[item.name][1] += 1
-    else
-      @items[item.name] = [item, 1]
+  def add_item(item, qty:1)
+    qty.times do
+      if @items.keys.include? item.name
+        @items[item.name][1] += 1
+      else
+        @items[item.name] = [item, 1]
+      end
     end
     retotal()
   end
@@ -75,21 +78,41 @@ class Bill
   ## Calculates the subtotal based on the content of @items
   def retotal
     @subtotal -= @subtotal
-    @items.each do |item|
+    @items.each do |key, item|
+      qty, item = item[1], item[0]
+      price = item.price
+      tax = item.tax / 100.00
+      dsc_quantity = 0
+      dsc_amount = Money.new(0, @eos.ccy)
+      new_total = Money.new(0, @eos.ccy)
+      puts "Price #{price}", "Qty #{qty}", "Tax #{tax}"
+      # Remove VAT from price if present
+      if item.price_include_vat
+        price = price / (1 + tax)
+        puts "Repriced #{price}"
+      end
       # Check if a discount is to be applied
+      if item.discount && item.discount[0] >= qty
+      end
+      # Total up price * quantity
+      tax = price * tax
+      @tax += tax
+      puts "New tax #{@tax}"
+      @subtotal += (price * qty + tax)
     end
   end
   
   def submit
     unless @submitted
-      eos.submit(self.clone)
+      retotal()
+      @eos.submit(self.clone)
       @submitted = true
     else
       puts "This bill has already been submitted."
     end
   end
   
-  attr_reader :items, :bill_ref, :subtotal, :submitted
+  attr_reader :items, :bill_ref, :subtotal, :submitted, :tax
 end
 
 # ITEM for purchase through the EOS system
@@ -100,10 +123,10 @@ end
 #                - The second integer (y) is the discount to be applied
 #                - The third integer is either - 0 - Buy x get y free (y is a quantity)
 #                                              - 1 - Buy x get y off  (y is an amount in pence)
-#   tax      - Must be a percentage
+#   tax      - Must be a decimal representing a percentage e.g. 17, 17%, 11.5,etc
 #   tags     - Must be a string or array of strings
 class Item
-  def initialize(eos, name, price, discount, tax=0, tags=[], price_include_vat=true)
+  def initialize(eos, name, price, discount, tax, tags, price_include_vat)
     @eos = eos
     @name = name
     @price = price
@@ -132,19 +155,24 @@ class Item
     @tags.map! {|tag| tosymbol(tag)}
     # Turn the name to a symbol
     @name = tosymbol(@name)
-    # Make price a money object
+    # Make price and tax money objects
     begin
       # Money silently turns invalid values to 0
       # so we'll check price is valid using Float
-      Float(@price.gsub(',', ''))
-      @price = Money.new(@price.gsub(',', ''), @eos.ccy)
+      @price = @price.to_s.gsub!(',', '') || @price.to_s
+      @tax = @tax.to_s.gsub!('%', '') || @tax.to_s
+      Float(@price); nil
+      Float(@tax); nil
+      @price = @price.to_i * Money::Currency.table[@eos.ccy.to_sym][:subunit_to_unit]
+      @price = Money.new(@price, @eos.ccy)
+      @tax = @tax.to_i
     rescue
-      raise Exception, "Bad value for 'price' %s" % price
+      raise Exception, "Bad value for price '#{@price}' or tax '#{@tax}'"
     end
   end
   
   private :validate, :tosymbol
-  attr_accessor :name, :price, :discount, :tax, :tags
+  attr_accessor :name, :price, :discount, :tax, :tags, :price_include_vat
 end
 
 
